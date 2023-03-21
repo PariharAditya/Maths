d8a726c0aa6e28fc0b7c0b0ea83f2aeecd0614ff
Aditya Parihar <adityapariharparihar@gmail.com>
2023-03-21 19:48:59 +0530

    Explanation

--- GrayCode.java
+++ GrayCode.java
@@ -10,7 +10,7 @@ static ArrayList<Integer> code(int n)
 {
 ArrayList ans=new ArrayList();
 for(int i=0;i<(1<<n);i++){
-  ans.add(i^(i>>1));
+  ans.add(i^(i>>1));   // Gray code sequence creation i>>1 means Right shifting or division by 2 then XORing(same = 0 different = 1)
 }
 return ans;
 }    
