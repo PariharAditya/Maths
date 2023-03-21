2d8a191a4cd81fb365170bf252e8ed5c7faa3e15
Aditya Parihar <adityapariharparihar@gmail.com>
2023-03-21 20:39:56 +0530

    life is too short to argue just ++ till they're continuous store count and move on

---
+++ ZeroSubArray.java
@@ -0,0 +1,25 @@
+public class ZeroSubArray 
+{
+public static void main(String[] args) 
+{
+int[] array = {1,3,0,0,2,0,0,4};
+System.out.println(subArray(array));
+}
+static long subArray(int[] nums)
+{
+long count = 0;
+long whenZeroStreakBreak = 0;
+for (int i = 0; i < nums.length; i++) 
+{
+if(nums[i]==0) count++;
+
+else count=0;
+
+/*Addition of 0's count when they were contiguous like 0,0 count would be 2
+ * and when the continuity break it get stored in result var  
+*/
+whenZeroStreakBreak+=count;  
+}    
+return whenZeroStreakBreak;
+}    
+}
